f3ed3f855b62db4f8643bd7cf6cc97c48718e79a
Rahul Soni <rahulsoni2225@gmail.com>
2021-10-07 19:16:03 +0530

    ssr styles done

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
-
+import { useStyles } from '../../utils';
 export const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Better Books</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar variant="dense">
           <Typography variant="h6" color="inherit" component="div">
             Better Books
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>All rights reserved 2021.</footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>All rights reserved 2021.</footer>
     </div>
   );
 };
